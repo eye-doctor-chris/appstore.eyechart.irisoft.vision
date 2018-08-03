2e9399ecc17940adb65aa8f251d6c4577b561817
Chris Butcher <chrisbutcher11@gmail.com>
2018-08-03 00:08:05 -0500

    Updates

--- platforms/amazon-fireos/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/amazon-fireos/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/Chris/Dev/eyechart.irisoft.vision/platforms/amazon-fireos/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/Chris/Dev/eyechart.irisoft.vision/platforms/amazon-fireos/CordovaLib/AndroidManifest.xml \
+/Users/Chris/Documents/GitHub/appstore.eyechart.irisoft.vision/platforms/amazon-fireos/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/Chris/Documents/GitHub/appstore.eyechart.irisoft.vision/platforms/amazon-fireos/CordovaLib/AndroidManifest.xml \
